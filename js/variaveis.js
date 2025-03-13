cc5a496ad8b1a782d3b195f4b8e2388ecbe19c87
amandachiovetolee <amandalee.dev2024@gmail.com>
2025-03-13 11:08:41 -0300

    exemplo 5

---
+++ js/variaveis.js
@@ -0,0 +1,11 @@
+function exemploVar(){
+    if (true){
+        //definir as variaveis
+        var x= 10;
+        let y= 20;
+        const z=30;
+    }
+    console.log(x); //acessivel
+    console.log(y); //erro: y não esta definido
+    console.log(zs); //erro: z não esta definido
+}
